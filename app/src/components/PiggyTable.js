1a60ca99e627efb163f107bb327e910c6e3c191b
Alex <cloudstrife79@gmail.com>
2019-05-18 13:57:27 -0400

    add wyre widget button poc

--- app/src/components/PiggyTable.js
+++ app/src/components/PiggyTable.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Card, CardBody, CardHeader, CardTitle, Container } from "reactstrap";
+import { Card, CardBody, CardHeader, CardTitle, Container, Button } from "reactstrap";
 
 import BootstrapTable from "react-bootstrap-table-next";
 import ToolkitProvider from "react-bootstrap-table2-toolkit";
@@ -57,6 +57,14 @@ const piggyColumns = [
   }
 ];
 
+function handleDAI() {
+  // spawn a modal and ask for an address
+
+  // add that address into this URL string: https://verify.testwyre.com/widget/v1?env=test&operation=debitcard&accountId=AC_1234&authType=metamask&destCurrency=DAI&sourceCurrency=USD&sourceAmount=0.01&dest=ethereum:0x4f5d52710C8C3E23e4c44f678868d23Aa2a49B37&redirectUrl=https://sendwyre.com
+
+  // open the modified URL w/ user-supplied ethereum address to send DAI to
+}
+
 class PiggyTable extends Component {
   constructor(props) {
     super(props)
@@ -94,9 +102,14 @@ class PiggyTable extends Component {
       <Card>
         <CardHeader>
           <CardTitle tag="h5">Piggies From Graph</CardTitle>
-          <h6 className="card-subtitle text-muted">
-            Filter Piggies using the controls on the left
-          </h6>
+          <div style={{float: 'left'}}>
+            <h6 className="card-subtitle text-muted">
+              Filter Piggies using the controls on the left
+            </h6>
+          </div>
+          <div style={{float: 'right'}}>
+            <Button color={'primary'} className="mr-1 mb-1" onClick={() => window.open('https://verify.testwyre.com/widget/v1?env=test&operation=debitcard&accountId=AC_1234&authType=metamask&destCurrency=DAI&sourceCurrency=USD&sourceAmount=0.01&dest=ethereum:0x4f5d52710C8C3E23e4c44f678868d23Aa2a49B37&redirectUrl=https://sendwyre.com', '_blank')}>Need DAI? Click Here</Button>
+          </div>
         </CardHeader>
         <CardBody>
         <BootstrapTable
